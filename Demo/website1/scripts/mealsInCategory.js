7195cdccfba91b39de2dada203684d409fca90e1
Etson <etsond@ufl.edu>
2024-06-03 15:07:30 -0400

    demo

---
+++ Demo/website1/scripts/mealsInCategory.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const apiBaseUrl = "https://www.themealdb.com/api/json/v1/1/filter.php?c=";
+
+window.onload = function () {
+    const getResultButton = document.getElementById("getResultsbutton");
+    getResultButton.onclick = onGetResultsButton;
+}
+
+function onGetResultsButton() {
+
+    console.log("We clicked");
+
+    const categoryInput = document.getElementById("categoryInput");
+    const resultsOutput = document.getElementById("resultsOutput");
+
+    let actualurl = apiBaseUrl + categoryInput.value;
+
+    fetch(actualurl).then(response => response.json()).then(data => {
+        console.log(data);
+
+        for (let meal of data.meals) {
+            let p = document.createElement("p");
+            p.innerHTML = meal.strMeal;
+
+            resultsOutput.appendChild(p);
+        }
+    })
+
+}
